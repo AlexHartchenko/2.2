36ef2cf43f26e0813e8fc24eacba9771c25493f0
Alex Hartchenko <aj.tomsk@mail.ru>
2022-03-08 18:16:09 +0700

    доработка 2

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,12 @@
 package ru.netology
 
 fun main() {
-    val likes = 23
+    val likes = 31
     println("Понравилось $likes ${likesPostFix(likes)}")
 }
 
 fun likesPostFix(likes: Int): String {
-    return if (likes % 2 == 1 && likes.toString().endsWith("11")) {
-        "людям"
-    } else if (likes.toString().endsWith("1")) {
+    return if (likes % 10 == 1 && !(likes.toString().endsWith("11"))) {
         "человеку"
     } else {
         "людям"
